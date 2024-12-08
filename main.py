53ec272601427f6d8a3b43ec32016499ce24f4ad
bubna92 <136169348+LPopivnyak@users.noreply.github.com>
2024-12-08 18:25:39 +0200

    Оформлення

--- main.py
+++ main.py
@@ -13,5 +13,21 @@ db_manager.create_table()
 #b_manager.add_question(2,1,"Більшість населення було зайняте в ")
 #db_manager.add_options(4,2,"Сільському господарстві",True)
 #db_manager.add_options(5,2,"Маштнобудівництві",False)
+#db_manager.add_quize(2,
+       #              "Квіз про Італію",
+        #        "деякі значення"
+        #        )
+#db_manager.add_quize(3,
+       #              "Квіз про Німечину",
+        #        "деякі значення"
+        #        )
+#db_manager.add_quize(4,
+       #              "Квіз про Іспанію",
+        #        "деякі значення"
+        #        )
+db_manager.add_quize(5,
+       #              "Квіз про Францію",
+        #        "деякі значення"
+        #        )
 
 
